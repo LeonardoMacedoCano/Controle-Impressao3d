7458279499731dea1eaaa8904877543e98d19d3d
LCano <leonardomacedocano@gmail.com>
2021-08-29 23:48:14 -0300

    add tabela parametrogeral

---
+++ atualizaBanco/versao1.sql
@@ -0,0 +1,11 @@
+-- Versão 2
+#
+CREATE TABLE parametrogeral (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT
+                      NOT NULL,
+    descricao STRING  NOT NULL,
+    valor     STRING,
+    parametro STRING  NOT NULL
+)
+#
+-- Fim
