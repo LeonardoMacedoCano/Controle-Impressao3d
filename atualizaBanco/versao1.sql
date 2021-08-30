7bdc531a1fff07c1dd3f1c69319f500443ff4f3f
LCano <leonardomacedocano@gmail.com>
2021-08-29 23:19:49 -0300

    add parametro Custo Manutencao

--- atualizaBanco/versao1.sql
+++ atualizaBanco/versao1.sql
@@ -3,3 +3,6 @@ VALUES('CustoEnergiaKWH', 'Custo Energia (kw/h)')
 #
 INSERT INTO parametrogeral (parametro, descricao)
 VALUES('ConsumoEnder3KWH', 'Consumo Ender 3 (kw/h)')
+#
+INSERT INTO parametrogeral (parametro, descricao)
+VALUES('CustoManutencao', 'Custo Manutenção (%)')
