95d3caa46266169cfb01d6a0b3397c2769649e40
LCano <leonardomacedocano@gmail.com>
2021-08-29 23:49:14 -0300

    add tabela tipofilamento

--- atualizaBanco/versao1.sql
+++ atualizaBanco/versao1.sql
@@ -8,4 +8,13 @@ CREATE TABLE parametrogeral (
     parametro STRING  NOT NULL
 )
 #
+CREATE TABLE tipofilamento (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT
+                      NOT NULL,
+    descricao STRING  NOT NULL,
+    valor     DOUBLE  NOT NULL,
+    densidade DOUBLE  NOT NULL,
+    diametro  DOUBLE  DEFAULT (1.75) 
+)
+#
 -- Fim
