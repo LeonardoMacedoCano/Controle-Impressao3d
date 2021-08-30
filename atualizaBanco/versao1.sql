e27eb22526c4a4cf1b5c835c9edf04c647c97752
LCano <leonardomacedocano@gmail.com>
2021-08-29 23:05:36 -0300

    add parametro Consumo Ender 3 kwh

--- atualizaBanco/versao1.sql
+++ atualizaBanco/versao1.sql
@@ -1,2 +1,5 @@
 INSERT INTO parametrogeral (parametro, descricao)
 VALUES('CustoEnergiaKWH', 'Custo Energia (kw/h)')
+#
+INSERT INTO parametrogeral (parametro, descricao)
+VALUES('ConsumoEnder3KWH', 'Consumo Ender 3 (kw/h)')
