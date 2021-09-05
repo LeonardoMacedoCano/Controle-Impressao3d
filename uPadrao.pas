7d9315b3d3ff07d575ba15d4737f0c7ab6683cf2
LCano <leonardomacedocano@gmail.com>
2021-09-05 01:41:10 -0300

    add frmPadrao

---
+++ uPadrao.pas
@@ -0,0 +1,26 @@
+unit uPadrao;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls, Vcl.ToolWin,
+  System.ImageList, Vcl.ImgList, Vcl.BaseImageCollection, Vcl.ImageCollection;
+
+type
+  TfrmPadrao = class(TForm)
+    imgToolBar: TImageList;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPadrao: TfrmPadrao;
+
+implementation
+
+{$R *.dfm}
+
+end.
