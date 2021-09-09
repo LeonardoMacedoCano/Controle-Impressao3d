cc26c55cbd0bb35764e0b740b33a1729bdfeaf14
LCano <leonardomacedocano@gmail.com>
2021-09-08 23:23:02 -0300

    add componentes visuais pesquisa padrao

--- uPesquisaPadrao.pas
+++ uPesquisaPadrao.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Buttons, Vcl.StdCtrls,
+  Data.DB, Vcl.Grids, Vcl.DBGrids;
 
 type
   TfrmPesquisaPadrao = class(TForm)
+    pnlFiltro: TPanel;
+    cbNomeColuna: TComboBox;
+    cbTipoFiltro: TComboBox;
+    edtFiltro: TEdit;
+    btnFiltrar: TButton;
+    btnLimparFiltro: TSpeedButton;
+    listBoxFiltros: TListBox;
+    DBGrid: TDBGrid;
   private
     { Private declarations }
   public
