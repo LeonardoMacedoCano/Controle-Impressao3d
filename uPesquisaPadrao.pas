913fdd1660e013dbe410961bdaff69e18d957e96
LCano <leonardomacedocano@gmail.com>
2021-09-08 23:10:26 -0300

    add frmPesquisaPadrao

---
+++ uPesquisaPadrao.pas
@@ -0,0 +1,24 @@
+unit uPesquisaPadrao;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TfrmPesquisaPadrao = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPesquisaPadrao: TfrmPesquisaPadrao;
+
+implementation
+
+{$R *.dfm}
+
+end.
