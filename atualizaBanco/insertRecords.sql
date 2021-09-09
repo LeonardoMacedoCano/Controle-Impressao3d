2f029592da109debd41e01b21f00a3b53bf55cf4
LCano <leonardomacedocano@gmail.com>
2021-09-08 23:53:00 -0300

    renomear versao2.sql para insertRecords.sql

--- atualizaBanco/insertRecords.sql
+++ atualizaBanco/insertRecords.sql
@@ -1,15 +1,13 @@
--- Versão 2
-#
+-- Insert Records
 INSERT INTO parametrogeral (parametro, descricao)
 VALUES('CustoEnergiaKWH', 'Custo Energia (kw/h)')
-#
+
 INSERT INTO parametrogeral (parametro, descricao)
 VALUES('ConsumoEnder3KWH', 'Consumo Ender 3 (kw/h)')
-#
+
 INSERT INTO parametrogeral (parametro, descricao)
 VALUES('CustoManutencao', 'Custo Manutenção (%)')
-#
+
 INSERT INTO parametrogeral (parametro, descricao)
 VALUES('ValorTopoManutencao', 'Valor Topo Manutenção')
-#
--- Fim
+-- end
